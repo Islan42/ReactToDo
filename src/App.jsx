f7659ec052caa0ad09201f5a5f1729faa726ee7e
Islan Victhor <islan12victhor@gmail.com>
2023-05-01 10:52:13 -0300

    NewTodoForm working as expected

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,39 @@ import { useState } from 'react'
 
 export default function App() {
   const [todos, setTodos] = useState(initialTodos)
-  const [filter, setFilter] = useState('All')
+  const [filter, setFilter] = useState('Todos')
+  const [newTodoString, setNewTodoString] = useState('')
+  
+  function newTodoHandler(todoString) {
+    const newTodo = {id: todos.length, string: todoString, finished: false}
+    setTodos([...todos, newTodo])
+  }
 
   return (
     <>
-      <NewToDo />
-      <Filter />
+      <NewToDoForm inputText={newTodoString} onChangeInput={setNewTodoString} onInsertTodo={newTodoHandler}/>
+      <Filter onChangeFilter={setFilter} />
       <TodoList todos={todos}/>
     </>
   )
 }
 
-function NewToDo() {
+function NewToDoForm({ inputText, onChangeInput, onInsertTodo }) {
+  function submitHandler(event) {
+    event.preventDefault()
+    onInsertTodo(inputText)
+    onChangeInput('')
+  }
+  
   return (
-    <form>
-      <input type="text" placeholder="A fazer" />
+    <form onSubmit={submitHandler}>
+      <input type="text" placeholder="A fazer" value={inputText} onChange={(event) => onChangeInput(event.target.value)}/>
       <input type="submit" value="Inserir" />
     </form>
   )
 }
 
-function Filter() {
+function Filter({ onChangeFilter }) {
   return (
     <form>
       <input type="radio" name="filter" value="Todos" />
@@ -54,3 +66,8 @@ const initialTodos = [
   {id: 1, string: 'Jogar Lol', finished: false}, 
   {id: 2, string: 'Estudar', finished: false}
 ]
+
+
+/* A FAZERES
+  [] Focar no input após inserir um novo item
+*/
